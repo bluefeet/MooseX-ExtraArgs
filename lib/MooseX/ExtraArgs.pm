89dc7b230a7bc918e53c8acc407df3e31f19bdf1
Aran Deltac <bluefeet@gmail.com>
2019-02-17 11:12:45 -0800

    Migrate all the tests from Test::More to Test2::V0.

--- lib/MooseX/ExtraArgs.pm
+++ lib/MooseX/ExtraArgs.pm
@@ -1,6 +1,6 @@
 package MooseX::ExtraArgs;
-use Moose ();
-use Moose::Exporter;
+
+$MooseX::ExtraArgs::VERSION = '0.01';
 
 =head1 NAME
 
@@ -28,6 +28,9 @@ This could be useful for proxy classes that expect extra arguments that will the
 used to pass as arguments to the underlying implementation.
 
 =cut
+
+use Moose ();
+use Moose::Exporter;
 
 Moose::Exporter->setup_import_methods(
     role_metaroles => {
